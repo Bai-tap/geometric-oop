491ae67ad97ef146d4406a78be75f3938e6bfc10
longhoang94 <longsutls@gmail.com>
2018-08-28 19:32:01 +0700

    add super class Geometric

---
+++ src/main/java/geomatric/Geometric.java
@@ -0,0 +1,39 @@
+package geomatric;
+
+
+public class Geometric {
+    private String color;
+    private String filled;
+
+    public Geometric() {
+    }
+
+    public Geometric(String color, String filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getFilled() {
+        return filled;
+    }
+
+    public void setFilled(String filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        return "Geometric{" +
+                "color='" + color + '\'' +
+                ", filled='" + filled + '\'' +
+                '}';
+    }
+}
